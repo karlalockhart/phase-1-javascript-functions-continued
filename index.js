19baabe622c3c591bcd9f07f24eca4421b20f5e3
Karla Lockhart <kblockhart@gmail.com>
2023-09-20 16:53:19 -0700

    made changes to index.js & completed lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // code your solution here
+/*function saturdayFun(activity = 'roller-skate') {
+    console.log(`This Saturday, I want to ${activity}`);
+    return `roller-skate`;
+}
+    saturdayFun();
+
+function mondayWork(activity = 'go to the office') {
+    console.log(`This Monday, I will ${activity}`);
+    return `go to the office`;
+}
+    mondayWork();
+*/
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+    }
+    saturdayFun()
+    
+    let mondayWork = function(activity="go to the office"){
+     return `This Monday, I will ${activity}.` 
+    }
+    mondayWork()
+    
+    function wrapAdjective (style="*") {
+      return function(stringOne="special"){
+        return `You are ${style}${stringOne}${style}!`
+      }
+    }
+    
+    let Calculator = {
+      add: function (a,b){
+        return a+b
+      } ,
+      subtract: (a,b) => {
+        return a - b
+      } ,
+      multiply: (a,b) => {
+        return a * b
+      } ,
+      divide: (a,b) => {
+        return a / b
+      } ,
+    }
+    
+    
+    function actionApplyer (start, array){
+      let a = start
+        for(let i = 0; i < array.length; i++){
+           a = array[i](a)
+          }
+        return a
+    }
